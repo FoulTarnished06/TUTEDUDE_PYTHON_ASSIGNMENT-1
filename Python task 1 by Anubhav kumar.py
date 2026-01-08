9fd3c0ecf53db5cfcfb9029d9b164e2f3fff1fe8
FoulTarnished06 <asrtheasr0112358@gmail.com>
2026-01-09 03:28:51 +0530

    Rename variables in mathematical operations for better clarity

--- Python task 1 by Anubhav kumar.py
+++ Python task 1 by Anubhav kumar.py
@@ -1,14 +1,15 @@
 #Python task 1 by Anubhav kumar
 #task 1 - Perform Basic Mathematical Operations
 
-n1 = int(input("Enter first number : "))
-n2 = int(input("Enter second number : "))
+number1 = int(input("Enter first number : "))
+number2 = int(input("Enter second number : "))
 
-print("addition : " , n1+n2)
-print("subtraction : " , n1-n2)
-print("multiplication : " , n1*n2)
+print("addition : " , number1+number2)
+print("subtraction : " , number1-number2)
+print("multiplication : " , number1*number2)
 
-if n2 == 0:
+if number2 == 0:
     print("Not defined")
 else:
-    print("division : " , n1/n2)
+
+    print("division : " , number1/number2)
